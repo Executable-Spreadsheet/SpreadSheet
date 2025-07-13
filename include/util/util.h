95066391982bdc2a875fd5d04d2a893bd4aa1626
Elijah Hantman <elihantman@gmail.com>
2025-07-13 16:15:38 -0700

    box drawing and started work on displaying values

--- include/util/util.h
+++ include/util/util.h
@@ -226,4 +226,9 @@ typedef struct v2u {
 u64 hash(u8* buf, u64 size);
 
 
+#define KB(x) (x * 1024)
+#define MB(x) (x * 1024 * 1024)
+#define GB(x) (x * 1024 * 1024 * 1024)
+
+
 #endif
--- src/parasheet-editor/main.c
+++ src/parasheet-editor/main.c
@@ -1,7 +1,9 @@
+#include "libparasheet/lib_internal.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <util/util.h>
 #include <ncurses.h>
+
 
 /*
 +---------------------------------------------------------------+
@@ -13,12 +15,28 @@
 +---------------------------------------------------------------+
 */
 
+#define CELL_WIDTH 10
+#define CELL_HEIGHT 2
+
+
+
+void drawBox(v2u pos, v2u size, SString s);
+SString CellDisplay(Allocator a, SpreadSheet* s, v2u pos);
+
+
 int main(int argc, char* argv[]) {
     initscr();
     noecho();               //prevent echoing output
     raw();                  //remove buffering
     keypad(stdscr, TRUE);   //enable extended keys
     set_escdelay(0);        //remove delay when processing esc
+    curs_set(0);            //set cursor to be invisible
+
+
+    SpreadSheet sheet = {
+        .mem = GlobalAllocatorCreate(),
+    };
+    Allocator stack = StackAllocatorCreate(sheet.mem, KB(1));
 
     clear();
     u8 c = 0;
@@ -26,9 +44,13 @@ int main(int argc, char* argv[]) {
         clear();
         //rendering
 
-        printw("Hello World!\n");
-        move(1, 0);
-        printw("Current Screen size: (%d, %d)", LINES, COLS);
+        v2u base = {0, 0};
+        for (u32 i = 0; i < COLS/CELL_WIDTH + 1; i++) {
+            for (u32 j = 0; j < LINES/CELL_HEIGHT + 1; j++) {
+                SString info = CellDisplay(stack, &sheet, (v2u){base.x + i, base.y + j});
+                drawBox((v2u){i * CELL_WIDTH, j * CELL_HEIGHT}, (v2u){CELL_WIDTH, CELL_HEIGHT}, info);
+            }
+        }
 
         refresh();
 
@@ -39,6 +61,34 @@ int main(int argc, char* argv[]) {
 
 
     endwin();
+
     return 0;
 }
 
+SString CellDisplay(Allocator a, SpreadSheet* s, v2u pos) {
+    CellValue* c = SpreadSheetGetCell(s, pos);
+
+    if (!c) {
+        return (SString){NULL, 0};
+    }
+
+    todo();
+
+}
+
+void drawBox(v2u pos, v2u size, SString s) {
+    
+    mvaddch(pos.y + 0,      pos.x + 0, '+');
+    mvaddch(pos.y + 0,      pos.x + size.x, '+');
+    mvaddch(pos.y + size.y, pos.x + 0, '+');
+    mvaddch(pos.y + size.y, pos.x + size.x, '+');
+
+    mvprintw(pos.y + size.y/2, pos.x + 1, "%.*s", s.size, s.data);
+
+    mvhline(pos.y + 0,      pos.x + 1, '-', size.x - 1);
+    mvhline(pos.y + size.y, pos.x + 1, '-', size.x - 1);
+
+    mvvline(pos.y + 1, pos.x + 0,      '|', size.y - 1);
+    mvvline(pos.y + 1, pos.x + size.x, '|', size.y - 1);
+
+}
