c3c98952f05a55e412799716a4a2b5ca16e130ac
cceerla <cceerla@ucsc.edu>
2025-07-21 15:57:30 -0700

    test is sad??

--- tests/libparasheet/parser_ordering_requirement.c
+++ tests/libparasheet/parser_ordering_requirement.c
@@ -7,13 +7,13 @@
 
 #define EPS UINT32_MAX
 
-void testString(char* input, StringTable* table, Allocator allocator){
-	TokenList* tokens = Tokenize(input, allocator);
-	AST* ast = BuildASTFromTokens(tokens, allocator);
-	if (ast->size > 0){
-		ASTPrint(stdout, ast);
-		for (int i = 0; i < ast->size - 1; i++){
-			ASTNode node = ast->nodes[i];
+void testString(char** input, StringTable* table, Allocator allocator){
+	TokenList* tokens = Tokenize(*input, table, allocator);
+	AST ast = BuildASTFromTokens(tokens, allocator);
+	if (ast.size > 0){
+		ASTPrint(stdout, &ast);
+		for (int i = 0; i < ast.size - 1; i++){
+			ASTNode node = ast.nodes[i];
 			assert(node.lchild == EPS || node.lchild < i);
 			assert(node.mchild == EPS || node.mchild < i);
 			assert(node.rchild == EPS || node.rchild < i);
@@ -21,7 +21,8 @@ void testString(char* input, StringTable* table, Allocator allocator){
 	} else {
 		assert(0);
 	}
-    ASTFree(&ast);
+	AST* astptr = &ast;
+    ASTFree(&astptr);
 	DestroyTokenList(&tokens);
 }
 
@@ -51,8 +52,8 @@ int main() {
 //		log("AST:");
 //		ASTPrint(stdout, &ast);
 //	}
-	
-	testString("=2+2", &s, allocator);
+	char* input1 = "=2+2";	
+	testString(&input1, &s, allocator);
 
     StringFree(&s);
 	return 0;
