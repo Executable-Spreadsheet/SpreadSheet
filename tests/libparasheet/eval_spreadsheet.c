ea7553d4a1f754ad84bfb073ecfbbf3d006af15b
Quincy Strange <qstrange@ucsc.edu>
2025-07-23 15:15:48 -0700

    BUT COMPILER ERRORS CAN
    And I fixed them

--- tests/libparasheet/eval_spreadsheet.c
+++ tests/libparasheet/eval_spreadsheet.c
@@ -1,6 +1,6 @@
-#include <libparasheet/tokenizer.h>
-#include <libparasheet/lib_internal.h>
 #include <libparasheet/evaluator.h>
+#include <libparasheet/lib_internal.h>
+#include <libparasheet/tokenizer.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,15 +11,24 @@
 #define PRINT_TOKENS 1
 #define PRINT_AST 1
 
-void testString(char* input, CellValue expected,
-		StringTable* strTable, SymbolTable* symTable, Allocator allocator){
+void testString(char* input, CellValue expected, StringTable* strTable,
+				Allocator allocator) {
 	SpreadSheet testSheet = {
 		.mem = allocator,
 	};
 	SpreadSheet outputSheet = {
 		.mem = allocator,
 	};
-	
+	SymbolTable cymbals = {
+		.mem = allocator,
+	};
+
+	StrID code = StringAddS(strTable, sstring(input));
+	SpreadSheetSetCell(&testSheet, (v2u){.x = 0, .y = 0},
+					   (CellValue){.t = CT_TEXT, .d.index = code});
+
+	SymbolPushScope(&cymbals);
+
 	EvaluateCell((EvalContext){
 		.srcSheet = &testSheet,
 		.inSheet = &outputSheet,
@@ -28,24 +37,39 @@ void testString(char* input, CellValue expected,
 		.currentY = 0,
 		.mem = allocator,
 		.str = strTable,
-		.table = symTable,
+		.table = &cymbals,
 	});
-	
+
+	CellValue* outputValue =
+		SpreadSheetGetCell(&outputSheet, (v2u){.x = 0, .y = 0});
+
 	// assert cell value is equal to expected
-	assert();
+	assert(outputValue->t == expected.t && outputValue->d.i == expected.d.i);
+
+	while (cymbals.size) {
+		SymbolPopScope(&cymbals);
+	}
+	Free(allocator, cymbals.scopes, cymbals.cap * sizeof(cymbals.scopes[0]));
+
+	SpreadSheetFree(&testSheet);
+	SpreadSheetFree(&outputSheet);
+
+	StringDel(strTable, code);
 }
 
 // Optional helper
 int main() {
 	Allocator allocator = GlobalAllocatorCreate();
-    StringTable s = {
-        .mem = allocator,
-    };
-	testString("=2+2;", &s, allocator);
-	testString("=2+2;3+3;", &s, allocator);
-	testString("=let x : int = 2; let y : int = 2; x + y;", &s, allocator);
-	testString("=let x : int = 3; let y: int = 4; 2 * (x + y);", &s, allocator);
+	StringTable s = {
+		.mem = allocator,
+	};
+	testString("=2+2;", (CellValue){.t = CT_INT, .d.i = 4}, &s, allocator);
+	testString("=2+2;3+3;", (CellValue){.t = CT_INT, .d.i = 6}, &s, allocator);
+	testString("=let x : int = 2; let y : int = 2; x + y;",
+			   (CellValue){.t = CT_INT, .d.i = 4}, &s, allocator);
+	testString("=let x : int = 3; let y: int = 4; 2 * (x + y);",
+			   (CellValue){.t = CT_INT, .d.i = 14}, &s, allocator);
 
-    StringFree(&s);
+	StringFree(&s);
 	return 0;
 }
